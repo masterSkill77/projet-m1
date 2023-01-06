1265d889e80ae0fd84fec2f14653da72651467d7
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-01-06 08:44:57 +0300

    feat service event

---
+++ app/Services/EventService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Event;
+use Illuminate\Database\Eloquent\Collection;
+
+class EventService
+{
+    public function __construct(public Event $eventModel)
+    {
+    }
+    public function getAllEvent(): Collection
+    {
+        return $this->eventModel::with('event_type')->get();
+    }
+    public function getByEventType(int $event_type): Collection
+    {
+        return $this->eventModel::with('event_type')->where('event_type', '==', $event_type)->get();
+    }
+
+    public function createEvent($data): Event
+    {
+        $event = new Event($data);
+        $event->save();
+        return $event;
+    }
+}
