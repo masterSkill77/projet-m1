76ec27ff443135d3cbdbc811e4158bcfddd34beb
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-01-07 19:20:25 +0300

    feat : add login on page

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::prefix('/evenement')->group(function () {
+Route::prefix('/evenement')->middleware('auth:sanctum')->group(function () {
     Route::get('/', [EventController::class, 'getAll']);
     Route::post('/', [EventController::class, 'createEvent']);
     Route::get('/type/{idType}', [EventController::class, 'getEventByType']);
@@ -33,30 +33,30 @@ Route::prefix('/evenement')->group(function () {
     Route::get('between-date/{date_start}/{date_end}', [EventController::class, 'getEventBetweenDate']);
 });
 
-Route::prefix('/partenaire')->group(function () {
+Route::prefix('/partenaire')->middleware('auth:sanctum')->group(function () {
     Route::get('/', [PartenaireController::class, 'getAll']);
     Route::post('/', [PartenaireController::class, 'createPartenaire']);
     Route::get('/{id}', [PartenaireController::class, 'getOne']);
 });
 
 
-Route::prefix('/offre')->group(function () {
+Route::prefix('/offre')->middleware('auth:sanctum')->group(function () {
     Route::get('/', [OffreController::class, 'getAll']);
     Route::post('/', [OffreController::class, 'createOffre']);
     Route::get('/{id}', [OffreController::class, 'getOne']);
 });
 
 
-Route::prefix('/information')->group(function () {
+Route::prefix('/information')->middleware('auth:sanctum')->group(function () {
     Route::get('/', [InformationController::class, 'getAll']);
     Route::post('/', [InformationController::class, 'createInfo']);
     Route::get('/{id}', [InformationController::class, 'getOne']);
     Route::get('/type/{id}', [InformationController::class, 'getInfoByType']);
 });
 
-Route::prefix('/information-type')->group(function () {
-    Route::get('/', [InformationTypeController::class, 'getAllType'])->middleware('auth:sanctum');
-    Route::post('/', [InformationTypeController::class, 'createType'])->middleware('auth:sanctum');
+Route::prefix('/information-type')->middleware('auth:sanctum')->group(function () {
+    Route::get('/', [InformationTypeController::class, 'getAllType']);
+    Route::post('/', [InformationTypeController::class, 'createType']);
 });
 
 Route::post('/login', LoginController::class);
